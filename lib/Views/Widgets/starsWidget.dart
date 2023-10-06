0b5736f2da99578966784b981d3798c68f844c5f
axelabbas <Fadeloy45@gmail.com>
2023-10-06 16:55:56 +0300

    cleaning

--- lib/Views/Widgets/starsWidget.dart
+++ lib/Views/Widgets/starsWidget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 starsWidget(double stars, double size, Color color) {
-  print(stars);
   return RatingBarIndicator(
       itemSize: size,
       itemBuilder: (context, index) => Icon(
@@ -10,47 +9,4 @@ starsWidget(double stars, double size, Color color) {
             color: color,
           ),
       rating: stars);
-  return RatingBar(
-    initialRating: stars,
-    ignoreGestures: true,
-    allowHalfRating: true,
-    onRatingUpdate: (value) {},
-    ratingWidget: RatingWidget(
-        full: Icon(
-          Icons.star,
-        ),
-        half: Icon(
-          Icons.star_half,
-        ),
-        empty: Icon(
-          Icons.star_border,
-        )),
-  );
 }
-// starsWidget(double stars, double size, Color color) {
-//     List<Widget> starsList = [];
-//     for (double i = 0; i < 5; i++) {
-//       if (stars - i >= 1) {
-//         starsList.add(Icon(
-//           Icons.star,
-//           size: size,
-//           color: color,
-//         ));
-//       } else if (stars - i <= 0) {
-//         starsList.add(Icon(
-//           Icons.star_border,
-//           size: size,
-//           color: color,
-//         ));
-//       } else {
-//         starsList.add(Icon(
-//           Icons.star_half,
-//           size: size,
-//           color: color,
-//         ));
-//       }
-//     }
-//     return Row(
-//       children: starsList,
-//     );
-//   }
